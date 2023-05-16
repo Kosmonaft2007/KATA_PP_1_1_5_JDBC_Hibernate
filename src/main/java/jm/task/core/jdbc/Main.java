be10eab3c4b35cdbc981d30e9307a81ef44c2da4
Vadim <kosmonaft2007@ya.ru>
2023-05-16 16:38:12 +0300

    устранение замечаний 3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,13 +11,19 @@ public class Main {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Ivan", "Nekrasov", (byte) 111);
+//        System.out.println("User c имненем '" + userService.getAllUsers().get(0).getName() + "' добавлен в базу данных");
         userService.saveUser("Anna", "Petrova", (byte) 11);
+//        System.out.println("User c имненем '" + userService.getAllUsers().get(1).getName() + "' добавлен в базу данных");
         userService.saveUser("Oleg", "Ivanov", (byte) 1);
+//        System.out.println("User c имненем '" + userService.getAllUsers().get(2).getName() + "' добавлен в базу данных");
         userService.saveUser("VV", "SS", (byte) 2);
+//        System.out.println("User c имненем '" + userService.getAllUsers().get(3).getName() + "' добавлен в базу данных");
+
+
         List<User> users = userService.getAllUsers(); // class User
         for (User user : users) {
 //            System.out.println(user);
-            System.out.println("User c имненем '" + user + "' добавлен в базу данных");
+            System.out.println("User c имненем '" + userService.getAllUsers().get(0).getName() + "' добавлен в базу данных");
         }
 
     }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -85,3 +85,33 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public void saveUser(String name, String lastName, byte age) throws SQLException {
+//        String dropTable = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?);";
+//
+//        try (PreparedStatement statement = connection.prepareStatement(dropTable)) {
+//            // Определяем значения переменных
+//            statement.setString(1, name);
+//            statement.setString(2, lastName);
+//            statement.setInt(3, age);
+//            statement.executeUpdate();
+//            connection.commit();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            connection.rollback();
+//        }
+//    }
